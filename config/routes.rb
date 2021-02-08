97fadc00df9a8d7f6d47af4a1739c03c283c1860
komusou.kamen <webengineer.komusou@gmail.com>
2021-02-08 22:59:42 +0000

    routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  get 'logout', to: 'sessions#destroy'
     
   get 'signup', to: 'users#new'
   resources :users, only: [:new, :create]
+  #post 'signup', to: 'users#new'??
   
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
