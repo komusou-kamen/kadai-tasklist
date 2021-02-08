aa64b16003d1e31ee90f681a7d2274d51a4a3454
komusou.kamen <webengineer.komusou@gmail.com>
2021-02-08 15:41:35 +0000

    add user to tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_06_093739) do
+ActiveRecord::Schema.define(version: 2021_02_08_153711) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.bigint "users_id"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+    t.index ["users_id"], name: "index_tasks_on_users_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -27,4 +31,6 @@ ActiveRecord::Schema.define(version: 2021_02_06_093739) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tasks", "users"
+  add_foreign_key "tasks", "users", column: "users_id"
 end
