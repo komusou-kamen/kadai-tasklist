461977ba5853effa8dc68afc9aa187b09cd1b394
komusou.kamen <webengineer.komusou@gmail.com>
2021-02-08 12:46:01 +0000

    toppage redirect to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root to: 'tasks#index'
   
-  get 'users/new'
-  post 'users/create'
-  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+    
   get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+  
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
