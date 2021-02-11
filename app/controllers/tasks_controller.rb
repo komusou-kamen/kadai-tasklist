20409f081c722d43320727acb7b44e2617037888
komusou.kamen <webengineer.komusou@gmail.com>
2021-02-11 11:15:07 +0000

    task delete correction

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   #before_action :set_task, only: [:update, :destroy]
-  before_action :correct_user, only: [:show, :edit, :update, :dastroy ]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy ]
   
   
   def index
